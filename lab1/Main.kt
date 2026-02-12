fa3fe6bf089d7d24f2b15720abb2227ad26c1976
alejandro <131113028+EPTalejandro@users.noreply.github.com>
2026-02-12 10:16:55 -0400

    actulizacion: creacion de archivo makefile y archivo main de prueba

---
+++ lab1/Main.kt
@@ -0,0 +1,41 @@
+fun main(){
+    
+    val grafo = ListaAdyacenciaGrafo<Int>()
+
+    grafo.agregarVertice(1)
+    grafo.agregarVertice(2)
+    grafo.agregarVertice(3)
+    grafo.agregarVertice(4)
+    grafo.agregarVertice(5) 
+
+    println(grafo.tamano())
+    println(grafo.contiene(2))
+
+    grafo.conectar(1,2)
+    grafo.conectar(1,3)
+    grafo.conectar(1,4)
+    grafo.conectar(1,5)
+    grafo.conectar(2,2)
+    grafo.conectar(2,3)
+    grafo.conectar(3,4)
+
+    println(grafo.obtenerArcosEntrada(4))
+    println(grafo.obtenerArcosSalida(2))
+    println(grafo.obtenerArcosSalida(1))
+
+    println(grafo.eliminarVertice(6))
+    grafo.eliminarVertice(3)
+    println(grafo.obtenerArcosSalida(2))
+    println(grafo.obtenerArcosSalida(1))
+
+    grafo.conectar(4,4)
+    grafo.conectar(5,1)
+    grafo.conectar(5,2)
+    grafo.conectar(5,5)
+    grafo.conectar(2,4)
+
+    val grafo2 = grafo.subgrafo(listOf(5,1))
+    println()
+    println(grafo.obtenerArcosSalida(5))
+    println(grafo.obtenerArcosSalida(1))
+}
